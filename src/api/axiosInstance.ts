87e8340ef45a5d4c8cf8fee87210839a820dabb0
Ahmed Imam <ahmedimam49@gmail.com>
2026-03-03 09:46:41 +0300

    chore: Update default API base URL

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://dev-qams-api.1techhub.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://qams-api.kleem.io/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
